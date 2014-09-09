44d8ed771fe5e649ba047bec75a4607a17315e9a
Anuj Garg <anujgarg1208@gmail.com>
2014-09-08 20:59:15 -0400

    Added accelerometer readings

--- src/edu/buffalo/cse/locationapp/MainActivity.java
+++ src/edu/buffalo/cse/locationapp/MainActivity.java
@@ -1,8 +1,11 @@
 package edu.buffalo.cse.locationapp;
 
-import android.support.v7.app.ActionBarActivity;
 import android.app.Activity;
 import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -11,21 +14,30 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
-import android.widget.Toast;
 
 
-public class MainActivity extends Activity implements LocationListener {
+public class MainActivity extends Activity implements LocationListener, SensorEventListener{
 LocationManager lm;
+Sensor mAccelerometerSensor;
+SensorManager sm;
+TextView tvGps, tvAccX, tvAccY, tvAccZ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvGps = (TextView)this.findViewById(R.id.displaytext);
+        tvAccX = (TextView)this.findViewById(R.id.xcoor);
+        tvAccY = (TextView)this.findViewById(R.id.ycoor);
+        tvAccZ = (TextView)this.findViewById(R.id.zcoor);
+        
         lm = (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
         lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0.0f, this);
         
+        sm = (SensorManager)this.getSystemService(SENSOR_SERVICE);
+        mAccelerometerSensor = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER); 
+        sm.registerListener(this, mAccelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
-
-
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -50,11 +62,7 @@ LocationManager lm;
 	public void onLocationChanged(Location location) {
 		// TODO Auto-generated method stub
 		Log.v("LocationApp","Location updated");
-		
-		TextView tv = (TextView)this.findViewById(R.id.displaytext);
-		tv.setText(location.getLatitude()+","+location.getLongitude());
-		tv.invalidate();
-		Toast.makeText(this, "Location update received: "+location.getLatitude()+","+location.getLongitude(), Toast.LENGTH_SHORT).show();
+		tvGps.setText(location.getLatitude()+", "+location.getLongitude());
 	}
 
 
@@ -74,6 +82,25 @@ LocationManager lm;
 
 	@Override
 	public void onProviderDisabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		// TODO Auto-generated method stub
+		Log.v("LocationApp", "Sensor changed");
+		if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+			tvAccX.setText("Acc. in X: " + event.values[0]);
+			tvAccY.setText("Acc. in Y: " + event.values[1]);
+			tvAccZ.setText("Acc. in Z: " + event.values[2]);
+		}
+	}
+
+
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
 		// TODO Auto-generated method stub
 		
 	}
