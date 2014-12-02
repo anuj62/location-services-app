8a53eb6ec39d382cb85d4163d8106ea958fdd12a
Anuj Garg <anujgarg1208@gmail.com>
2014-12-02 14:48:04 -0500

    added pedometer algorithm

---
+++ src/edu/buffalo/cse/algorithm/pedometer/PedometerPathMapperEventListener.java
@@ -0,0 +1,9 @@
+package edu.buffalo.cse.algorithm.pedometer;
+
+import android.graphics.PointF;
+
+public interface PedometerPathMapperEventListener {
+	
+	public void getFinalPath(Walk walk);
+	public void getLocation(PointF point);
+}
